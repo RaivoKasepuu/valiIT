97e1995d595481d2d2b6f73c213491e082cc736a
Raivo <raivokasepuu@gmail.com>
2020-08-26 15:31:19 +0300

    lesson3done

---
+++ src/Testjava.java
@@ -0,0 +1,8 @@
+public class Testjava {
+    public static void main(String[] args) {
+        //  System.out.println("tere Java");
+        System.out.println(String.format("Hello World, %s!", args[0])); //1
+        System.out.println("Hello World, " + args[0] + "!"); //2
+        System.out.println("Blaaaa");
+    }
+}
